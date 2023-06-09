518a71f7ee070013de7b3e3210925d67729d51eb
鹿胜宝 <lsb01622404@alibaba-inc.com>
2023-06-09 10:25:16 +0800

    feature：个人中心地址的增删改查

--- haohuo-service/haohuo-service-user/src/main/java/com/ebanma/cloud/user/UserServiceApplication.java
+++ haohuo-service/haohuo-service-user/src/main/java/com/ebanma/cloud/user/UserServiceApplication.java
@@ -1,13 +1,14 @@
 package com.ebanma.cloud.user;
 
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
-@MapperScan(basePackages = {"com.ebanma.cloud.user.dao", "com.ebanma.cloud.common.core"})
+@MapperScan(basePackages = {"com.ebanma.cloud.user.dao"})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
