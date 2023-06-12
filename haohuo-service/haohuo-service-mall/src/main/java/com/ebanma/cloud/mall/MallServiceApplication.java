219a529f41e864e1ad68d183ebb4dd1ed128161d
王海洋 <why01622413@alibaba-inc.com>
2023-06-12 08:30:28 +0800

    no message

--- haohuo-service/haohuo-service-mall/src/main/java/com/ebanma/cloud/mall/MallServiceApplication.java
+++ haohuo-service/haohuo-service-mall/src/main/java/com/ebanma/cloud/mall/MallServiceApplication.java
@@ -3,11 +3,13 @@ package com.ebanma.cloud.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 @SpringBootApplication
 //@EnableDiscoveryClient
+@EnableFeignClients({"com.ebanma.cloud.*.api.*"})
 public class MallServiceApplication {
 
     public static void main(String[] args) {
