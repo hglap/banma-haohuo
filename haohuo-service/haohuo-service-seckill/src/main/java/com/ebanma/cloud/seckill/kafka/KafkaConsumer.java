c3f08310d295b54417ddf768bf4a6c5a8ad3809c
崔国垲 <cgk01622426@alibaba-inc.com>
2023-06-12 17:37:35 +0800

    feature:调用游戏feign

--- haohuo-service/haohuo-service-seckill/src/main/java/com/ebanma/cloud/seckill/kafka/KafkaConsumer.java
+++ haohuo-service/haohuo-service-seckill/src/main/java/com/ebanma/cloud/seckill/kafka/KafkaConsumer.java
@@ -1,10 +1,15 @@
 package com.ebanma.cloud.seckill.kafka;
 
 import com.alibaba.fastjson.JSON;
+import com.ebanma.cloud.common.dto.Result;
 import com.ebanma.cloud.common.util.IdWorker;
 import com.ebanma.cloud.seckill.dao.SeckillMapper;
 import com.ebanma.cloud.seckill.model.dto.SeckillMessageDto;
 import com.ebanma.cloud.seckill.model.po.Seckill;
+import com.ebanma.cloud.seckill.service.FeignServiceGateway;
+import com.ebanma.cloud.trans.api.dto.RedPacket;
+import com.ebanma.cloud.trans.api.dto.TransAccountLog;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.BeanUtils;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -13,7 +18,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,21 +29,17 @@ import java.util.List;
  */
 @Service
 public class KafkaConsumer {
-    // 消费监听
-//    @KafkaListener(topics = "seckill")
-//    public void onMessage(ConsumerRecord<String, String> record) {
-//        //获取消费者组
-//        String groupId= KafkaUtils.getConsumerGroupId();
-//        // 消费的哪个topic、partition的消息,打印出消息内容
-//        System.out.println("简单消费：" +groupId+"-"+ record.topic() + "-" + record.partition() + "-" + record.value().toString());
-//    }
+
 
     @Resource
     private SeckillMapper seckillMapper;
 
+    @Resource
+    private FeignServiceGateway gameServiceGateway;
+
     private IdWorker idWorker;
 
-    @KafkaListener(topics = "seckill")
+//    @KafkaListener(topics = "seckill",id = "seckillMulti",groupId = "seckill_group")
     public void onMessage(List<String> records) {
         List<Seckill>  list = new ArrayList<>(records.size());
         for(String record : records){
@@ -50,9 +53,20 @@ public class KafkaConsumer {
             list.add(seckill);
         }
         seckillMapper.addBantch(list);
-//        //获取消费者组
-//        String groupId= KafkaUtils.getConsumerGroupId();
-//        // 消费的哪个topic、partition的消息,打印出消息内容
-//        System.out.println("简单消费：" +groupId+"-"+ record.topic() + "-" + record.partition() + "-" + record.value().toString());
+    }
+    @KafkaListener(topics = "seckill",groupId = "seckill_single_group",id = "seckillSingle")
+    public void onMessage(String record) {
+        SeckillMessageDto seckillMessageDto =JSON.parseObject(record,SeckillMessageDto.class);
+        TransAccountLog log = new TransAccountLog();
+        log.setUserId( seckillMessageDto.getUserId());
+//        log.setAmount(2);
+//        log.setLogType(0);
+//        log.setBizType(Integer bizType);
+//        log.setDescription(String "");
+//        log.setSource(String source);
+//        log.setCreateOn(Date createOn);
+//        log.setCreateBy(String createBy);
+//        log.setBizSerialNumber();
+//        Result r = gameServiceGateway.updateTrans(log);
     }
 }
